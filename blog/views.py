9dbc422570785e7b40f11b2d9359d702c47d5322
Dan <daniel.liemng@gmail.com>
2022-03-04 02:51:05 -0500

    retrieve posts from DB

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,31 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-  {
-    'author': 'David Tee',
-    'title': 'Blog Post 1',
-    'content': 'First post comment',
-    'date_posted': 'August 27, 2021'
-  },
-    {
-    'author': 'David Tee 2',
-    'title': 'Blog Post 2',
-    'content': 'Second post comment',
-    'date_posted': 'September 10, 2021'
-  },
-    {
-    'author': 'David Tee 3',
-    'title': 'Blog Post 3',
-    'content': 'Third post comment',
-    'date_posted': 'Dec 25, 2021'
-  },
-]
+# posts = [
+#   {
+#     'author': 'David Tee',
+#     'title': 'Blog Post 1',
+#     'content': 'First post comment',
+#     'date_posted': 'August 27, 2021'
+#   },
+#     {
+#     'author': 'David Tee 2',
+#     'title': 'Blog Post 2',
+#     'content': 'Second post comment',
+#     'date_posted': 'September 10, 2021'
+#   },
+#     {
+#     'author': 'David Tee 3',
+#     'title': 'Blog Post 3',
+#     'content': 'Third post comment',
+#     'date_posted': 'Dec 25, 2021'
+#   },
+# ]
 
 # Create your views here.
 def home(request):
   context = {
-    'posts': posts
+    'posts': Post.objects.all()
   }
 
   return render(request, 'blog/home.html', context)
