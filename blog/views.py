f1d108f1df4c94eb934c8eaafcb13d3f3943958a
Dan <daniel.liemng@gmail.com>
2022-03-14 11:52:54 -0400

    user posts with pagination

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,8 @@
-from dataclasses import fields
-from pyexpat import model
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 
 # Create your views here.
 # def home(request):
@@ -21,6 +20,21 @@ class PostListView(ListView):
   context_object_name = 'posts'
   ordering = ['-date_posted']
   paginate_by = 5
+
+
+class UserPostListView(ListView):
+  model = Post
+  # Default template path: <app>/<model>_<viewtype>.html
+  template_name = 'blog/user_posts.html'
+  context_object_name = 'posts'
+  ordering = ['-date_posted']
+  paginate_by = 5
+
+  def get_queryset(self):
+    # get username from URL
+    # if not exist, show 404 page
+    user = get_object_or_404(User, username=self.kwargs.get('username'))
+    return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
